d30d863a63881d25c1a19709703d02863b1ad2f1
Tony <t4tonykuriakose@gmail.com>
2025-04-15 19:02:32 +0530

    task

--- todo-service/src/routes/taskRoutes.ts
+++ todo-service/src/routes/taskRoutes.ts
@@ -1,18 +1,11 @@
 import express from "express";
-import {
-  validateCreateTask,
-  createTask,
-  getTasks,
-  updateTask,
-  deleteTask,
-} from "../controllers/taskController";
+import {validateCreateTask,createTask,getTasks,updateTask,deleteTask,} from "../controllers/taskController";
 import { authenticate } from "../middleware/authenticate"
 
 const router = express.Router();
 
 
-router.get("/tasks",async (req,res)=>{
-  res.json(tasks)},authenticate, getTasks);
+router.get("/tasks",authenticate, getTasks);
 router.post("/tasks",authenticate, validateCreateTask, createTask);
 router.put("/tasks/:id", authenticate, updateTask);
 router.delete("/tasks/:id", authenticate, deleteTask);
